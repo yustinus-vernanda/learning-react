ac38bb9e41230df255543a57a0a2594e0a912b71
yustinus-vernanda <yustinusv@gmail.com>
2021-06-23 14:55:38 +0700

    Cleaning Code

--- src/View/Home.js
+++ src/View/Home.js
@@ -4,39 +4,12 @@ import {useParams} from 'react-router-dom'
 import Loader from '../Components/Loader'
 import HelloWorld from '../Components/HelloWorld'
 import ProductCard from '../Components/ProductCard'
-
+import {useAxiosGet} from '../Hooks/HttpRequest'
 
 function Home(){
   let content = null
-    const url = `https://60cbfbd371b73400171f6cfb.mockapi.io/api/v1/products?page=1&limit=10`
-    const [products,setProducts]= useState({
-      loading: false,
-      data: null,
-      error:false
-    })
-
-  useEffect(()=>{
-      setProducts({
-          loading:true,
-          data:null,
-          error:false
-      })
-      axios.get(url)
-        .then(response=>{
-          setProducts({
-              loading: false,
-              data: response.data,
-              error:false
-          })
-        })
-        .catch(()=>{
-          setProducts({
-              loading:false,
-              data:null,
-              error:true
-          })
-        })
-    },[url])
+  const url = `https://60cbfbd371b73400171f6cfb.mockapi.io/api/v1/products?page=1&limit=10`
+  let products = useAxiosGet(url)
 
   if(products.loading){
     content = <Loader/>
--- src/View/Product.js
+++ src/View/Product.js
@@ -2,40 +2,19 @@ import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 import {useParams} from 'react-router-dom'
 import Loader from '../Components/Loader'
+import {useAxiosGet} from '../Hooks/HttpRequest'
 
 function Product(){
   const {id} = useParams()
   const url = `https://60cbfbd371b73400171f6cfb.mockapi.io/api/v1/products/${id}`
-  const [product,setProduct]= useState({
-    loading: false,
-    data: null,
-    error:false
-  })
+
+  let product = useAxiosGet(url)
+
+
   let content = null
 
 
-  useEffect(()=>{
-    setProduct({
-        loading:true,
-        data:null,
-        error:false
-    })
-    axios.get(url)
-      .then(response=>{
-        setProduct({
-            loading: false,
-            data: response.data,
-            error:false
-        })
-      })
-      .catch(()=>{
-        setProduct({
-            loading:false,
-            data:null,
-            error:true
-        })
-      })
-  },[url])
+
 
   if(product.loading){
     content = <Loader/>
